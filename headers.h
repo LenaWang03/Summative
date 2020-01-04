015df1ab7f12f20850d45e40bde69ad6f075c893
thesacredmoocow <thesacredmoocow@users.noreply.github.com>
2020-01-03 20:07:42 -0500

    sorted function prototypes

--- headers.h
+++ headers.h
@@ -49,36 +49,47 @@ struct Item {
     int amount;
 };
 
+///setUp.cpp
 void initializeAllegro();
-int checkSetup(ALLEGRO_DISPLAY *display, ALLEGRO_FONT *font, ALLEGRO_FONT *fontPixel, ALLEGRO_TIMER *timer, ALLEGRO_EVENT_QUEUE *event_queue);
 void setupLevel(LevelBG &a, Character &b, Object &c, int l, Item &p);
-void moveCharacter(Character &player, LevelBG b, storeCollision c, ALLEGRO_EVENT &ev, Object l);
-void calcBounds(Character &a);
-void calcObjectBounds(Object &a);
 void loadCharacterImage(Character &a);
 void loadObjectImage(Object &a);
+void getCharacter(Character &a);
+void getObjects(LevelBG &a, int l);
+void drawLives(Object a);
+int checkSetup(ALLEGRO_DISPLAY *display, ALLEGRO_FONT *font, ALLEGRO_FONT *fontPixel, ALLEGRO_TIMER *timer, ALLEGRO_EVENT_QUEUE *event_queue);
+
+///summativeAllegro.cpp
+void drawObjects(Object a[]);
+void drawObject(Object a);
+void drawBG(LevelBG a, Object &l);
+int playerAnimation (Character a);
+int enemyAnimation (Object a);
+void isHit(Character &a, LevelBG b, int hitCounter, Object &l);
+
+///fileFunctions.cpp
+void moveCharacter(Character &player, LevelBG b, storeCollision c, ALLEGRO_EVENT &ev, Object l);
+int getNewDir(storeCollision c, int curr);
+void moveEnemy(Object a[], LevelBG &b, ALLEGRO_EVENT &ev);
+bool endLevel(Character a, Object d);
+bool pickUpPetal (Character a, Object b, Item &c);
+
+///Collisions.cpp
+void calcBounds(Character &a);
+void calcObjectBounds(Object &a);
 storeCollision isBackgroundCollision(Character &a);
 storeCollision isObjectCollision(Character &a, Object b);
 storeCollision compareCollision(Character a, LevelBG b);
-void getCharacter(Character &a);
-void getObjects(LevelBG &a, int l);
-void drawObjects(Object a[]);
-void drawObject(Object a);
 void stopCollision (Character &player, storeCollision);
-void drawBG(LevelBG a, Object &l);
-bool endLevel(Character a, Object d);
-int playerAnimation (Character a);
-void moveEnemy(Object a[], LevelBG &b, ALLEGRO_EVENT &ev);
-storeCollision compareEnemyCollision(Object &a, Object b[], Object c[]);
-storeCollision isEnemyObjectCollision(Object &a, Object b);
 storeCollision isEnemyBackgroundCollision(Object &a);
-int enemyAnimation (Object a);
-void isHit(Character &a, LevelBG b, int hitCounter, Object &l);
-void drawLives(Object a);
+storeCollision isEnemyObjectCollision(Object &a, Object b);
+storeCollision compareEnemyCollision(Object &a, Object b[], Object c[]);
+
+///buttons.cpp
 bool makeButton (Button a, ALLEGRO_EVENT ev, ALLEGRO_FONT *fontPixel);
 void declareButtons(Button &start, Button &menu, Button &exitGame, Button &resume, Button &nextLevel);
-bool pickUpPetal (Character a, Object b, Item &c);
-int getNewDir(storeCollision c, int curr);
+
+
 
 
 
